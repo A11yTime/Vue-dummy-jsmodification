be28bb8ede26ce5137fddbf10feae1e17ccf6075
Md Rana <m.sohel.sw@gamil.com>
2023-06-08 06:46:33 -0400

    js modificaiton instead app import

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+<li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{ friend.phone }}</li>
+        <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+</li>
+</template>
+ <script>
+ export default {
+     data(){
+        return{
+            detailsAreVisible: false,
+            friend: {
+                id: 'Rubab',
+                name: 'Rubab Rana',
+                phone: '245 823 5874',
+                email: 'rubab@localhost.com'
+            }
+        }
+     },
+     methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+     }
+ }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
+import FriendContactVue from './components/FriendContact.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('friend-contact', FriendContactVue)
+
+app.mount('#app')
